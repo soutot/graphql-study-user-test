3b3227749bf28ffa1a35438e726aedafe5a7b400
Tiago Souto <soutot@users.noreply.github.com>
2017-06-16 00:39:24 -0300

    Resolving the rootquery and exporting the schema

--- users/server.js
+++ users/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 
 app.listen(4000, () => {
   app.use('/graphql', expressGraphQL({
+    schema, // schema: schema
     graphiql: true //é uma ferramenta que permite realizar queries em ambiente de desenvolvimento
   }))
   console.log('Listening');
