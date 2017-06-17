2595a582d3dc8799e32b9c889fc8154114efb383
Tiago Souto <soutot@users.noreply.github.com>
2017-06-17 01:30:38 -0300

    Added editUser mutation

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -31,7 +31,7 @@ const UserType = new GraphQLObjectType({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
-    // companyId: { type: GraphQLString },
+    companyId: { type: GraphQLString },
     company: {
       type: CompanyType,
       resolve(parentValue, args) {
@@ -76,6 +76,29 @@ const mutation = new GraphQLObjectType({
       },
       resolve(parentValue, { firstName, age }) {
         return axios.post('http://localhost:3000/users', { firstName, age })
+        .then(res => res.data);
+      }
+    },
+    deleteUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) }
+      },
+      resolve(parentValue, { id }) {
+        return axios.delete(`http://localhost:3000/users/${id}`)
+        .then(res => res.data);
+      }
+    },
+    updateUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString)},
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        companyId: { type: GraphQLString}
+      },
+      resolve(parentValue, args) {
+        return axios.patch(`http://localhost:3000/users/${args.id}`, args)
         .then(res => res.data);
       }
     }
