133519df9aff0f3d82c1c85c4c5c3f107d321ab6
Tiago Souto <soutot@users.noreply.github.com>
2017-06-16 23:27:06 -0300

    addUser mutation created

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -6,6 +6,7 @@ const {
   GraphQLInt,
   GraphQLSchema,
   GraphQLList,
+  GraphQLNonNull,
 } = graphql;
 
 const CompanyType = new GraphQLObjectType({
@@ -63,4 +64,25 @@ const RootQuery = new GraphQLObjectType({
   }
 });
 
-module.exports = new GraphQLSchema({ query: RootQuery })
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addUser: {
+      type: UserType,
+      args: {
+        firstName: { type: new GraphQLNonNull(GraphQLString) },
+        age: { type: new GraphQLNonNull(GraphQLInt) },
+        companyId: { type: GraphQLString }
+      },
+      resolve(parentValue, { firstName, age }) {
+        return axios.post('http://localhost:3000/users', { firstName, age })
+        .then(res => res.data);
+      }
+    }
+  }
+})
+
+module.exports = new GraphQLSchema({ 
+  query: RootQuery,
+  mutation
+})
